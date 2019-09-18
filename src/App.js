4c706c25bc12e14b84b58468340880acddfca4f0
Cat Chen <catchen@catchen.me>
2019-09-17 22:18:22 -0700

    Replaced App.js with input and button
    Replaced App.js with input and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography
+} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline>
+        <Container maxWidth="sm">
+          <Box m={4}>
+            <Typography variant="h3" component="h1">Trace URL</Typography>
+            <form>
+              <TextField
+                autoFocus={true}
+                fullWidth={true}
+                label="URL"
+                name="url"
+                placeholder="Any URL that redirects"
+                variant="filled"
+              />
+              <Button fullWidth={true} variant="contained" color="primary">
+                Trace
+              </Button>
+            </form>
+          </Box>
+        </Container>
+      </CssBaseline>
     </div>
   );
 }
