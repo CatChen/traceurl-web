4506e8009b6dd53964936b4377210635544ed586
Cat Chen <catchen@catchen.me>
2019-11-01 22:39:03 -0700

    Stopped ServiceWorker for crawlers (#92)

--- src/index.js
+++ src/index.js
@@ -10,4 +10,13 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+if (
+  navigator.userAgent.search(
+    /facebookexternalhit|facebot|twitterbot|googlebot/i,
+  ) >= 0
+) {
+  // avoid unintended caching in crawlers
+  serviceWorker.unregister();
+} else {
+  serviceWorker.register();
+}
