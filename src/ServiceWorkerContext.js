e6b10b44acc5cdbb67ef9cddd38c14e988334cb3
Cat Chen <catchen@catchen.me>
2019-11-04 10:09:23 -0800

    Service worker context (#112)
    * Created ServiceWorkerContext
    
    * Added snackbar to prompt service worker update
    
    * Fixed all Flow errors

---
+++ src/ServiceWorkerContext.js
@@ -0,0 +1,96 @@
+// @flow strict
+
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import type { AbstractComponent } from 'react';
+
+type ServiceWorkerContextValue = {|
+  updateAvailable: boolean,
+  cacheComplete: boolean,
+  applyUpdate: () => void,
+|};
+
+const ServiceWorkerContext = createContext<ServiceWorkerContextValue>({
+  updateAvailable: false,
+  cacheComplete: false,
+  applyUpdate: () => {},
+});
+
+export function withServiceWorkerContextProvider<Config: {}>(
+  Component: AbstractComponent<Config>,
+): AbstractComponent<Config> {
+  return (props: Config) => {
+    const [updateAvailable, setUpdateAvailable] = useState(false);
+    const [cacheComplete, setCacheComplete] = useState(false);
+    const [
+      waitingServiceWorker,
+      setWaitingServiceWorker,
+    ] = useState<?ServiceWorker>(null);
+
+    const value = useMemo(() => {
+      return {
+        updateAvailable,
+        cacheComplete,
+        applyUpdate: () => {
+          if (!waitingServiceWorker) {
+            return;
+          }
+          waitingServiceWorker.addEventListener('statechange', () => {
+            if (waitingServiceWorker.state === 'activated') {
+              window.location.reload();
+            }
+          });
+
+          waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+        },
+      };
+    }, [updateAvailable, cacheComplete, waitingServiceWorker]);
+
+    useEffect(() => {
+      (async () => {
+        if (navigator.serviceWorker) {
+          const serviceWorker = navigator.serviceWorker;
+          const registration = await serviceWorker.getRegistration('/');
+          if (!registration) {
+            return;
+          }
+
+          registration.addEventListener('updatefound', () => {
+            const installingWorker = registration.installing;
+            if (installingWorker == null) {
+              return;
+            }
+
+            installingWorker.addEventListener('statechange', () => {
+              if (installingWorker.state === 'installed') {
+                if (serviceWorker.controller) {
+                  setUpdateAvailable(true);
+                  setWaitingServiceWorker(registration.waiting);
+                } else {
+                  setCacheComplete(true);
+                }
+              }
+            });
+          });
+        }
+      })();
+      return () => {};
+    });
+
+    return (
+      <ServiceWorkerContext.Provider value={value}>
+        <Component {...props} />
+      </ServiceWorkerContext.Provider>
+    );
+  };
+}
+
+export function useServiceWorkerContext(): ServiceWorkerContextValue {
+  const context = useContext(ServiceWorkerContext);
+  return context;
+}
---
+++ src/ServiceWorkerStatus.js
@@ -0,0 +1,44 @@
+// @flow strict
+
+import React, { useState } from 'react';
+import { IconButton, Slide, Snackbar, Typography } from '@material-ui/core';
+import Update from '@material-ui/icons/UpdateOutlined';
+import {
+  withServiceWorkerContextProvider,
+  useServiceWorkerContext,
+} from './ServiceWorkerContext';
+
+import type { ElementConfig } from 'react';
+
+function ServiceWorkerStatus() {
+  const [open, setOpen] = useState(false);
+  const context = useServiceWorkerContext();
+
+  if (context.updateAvailable) {
+    setOpen(true);
+  }
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      TransitionComponent={Slide}
+      open={open}
+      message={<Typography component="span">Update available.</Typography>}
+      action={
+        <IconButton
+          color="inherit"
+          onClick={(event) => {
+            setOpen(false);
+            context.applyUpdate();
+          }}
+        >
+          <Update />
+        </IconButton>
+      }
+    />
+  );
+}
+
+export default withServiceWorkerContextProvider<ElementConfig<typeof Snackbar>>(
+  ServiceWorkerStatus,
+);
