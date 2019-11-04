db856521bff1e623766fa2e8689d4000587b31f0
Cat Chen <catchen@catchen.me>
2019-11-04 15:22:52 -0800

    Changed service worker events to Promise based

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
 window.addEventListener('beforeinstallprompt', async (event) => {
   const choiceResult = await event.userChoice();
   Analytics.logEvent('app', 'prompt', choiceResult.outcome);
@@ -17,7 +15,25 @@ window.addEventListener('appinstalled', (event) => {
   Analytics.logEvent('app', 'install', null, 1);
 });
 
+let resolveUpdateAvailable;
+let resolveCacheComplete;
+window.updateAvailable = new Promise((resolve) => {
+  resolveUpdateAvailable = resolve;
+});
+window.cacheComplete = new Promise((resolve) => {
+  resolveCacheComplete = resolve;
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: (registration) => {
+    resolveUpdateAvailable(registration);
+  },
+  onSuccess: (registration) => {
+    resolveCacheComplete(registration);
+  },
+});
