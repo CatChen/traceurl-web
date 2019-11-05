3410369b542d458e3056681b8dd52a3152ca554f
Cat Chen <catchen@catchen.me>
2019-11-04 16:04:04 -0800

    Fixed ServiceWorkerContext mount event (#120)

--- src/ServiceWorkerContext.js
+++ src/ServiceWorkerContext.js
@@ -105,7 +105,7 @@ export function withServiceWorkerContextProvider<Config: {}>(
         }
       })();
       return () => {};
-    });
+    }, []);
 
     return (
       <ServiceWorkerContext.Provider value={value}>
