6653aa11448330a4e794adc6e4291909fe6f2c6d
Cat Chen <catchen@catchen.me>
2019-11-05 17:03:18 -0800

    Set up status to display cache complete event (#124)

--- src/ServiceWorkerStatus.js
+++ src/ServiceWorkerStatus.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import { IconButton, Slide, Snackbar, Typography } from '@material-ui/core';
+import Close from '@material-ui/icons/Close';
 import SystemUpdate from '@material-ui/icons/SystemUpdateOutlined';
 import Analytics from './Analytics';
 import {
@@ -12,19 +13,23 @@ import {
 import type { ElementConfig } from 'react';
 
 function ServiceWorkerStatus() {
-  const [open, setOpen] = useState(false);
-  const context = useServiceWorkerContext();
+  return (
+    <>
+      <ServiceWorkerUpdateStatus />
+      <ServiceWorkerCacheStatus />
+    </>
+  );
+}
 
-  if (open !== context.updateAvailable) {
-    setOpen(context.updateAvailable);
-  }
+function ServiceWorkerUpdateStatus() {
+  const context = useServiceWorkerContext();
 
   return (
     <Snackbar
       anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
       TransitionComponent={Slide}
-      open={open}
-      message={<Typography component="span">Update available.</Typography>}
+      open={context.updateAvailable}
+      message={<Typography component="span">App update available.</Typography>}
       action={
         <IconButton
           color="inherit"
@@ -41,6 +46,37 @@ function ServiceWorkerStatus() {
   );
 }
 
-export default withServiceWorkerContextProvider<ElementConfig<typeof Snackbar>>(
-  ServiceWorkerStatus,
-);
+function ServiceWorkerCacheStatus() {
+  const [dismissed, setDismissed] = useState(false);
+  const context = useServiceWorkerContext();
+
+  const close = () => {
+    setDismissed(true);
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      TransitionComponent={Slide}
+      open={context.cacheComplete && !dismissed}
+      onClock={close}
+      message={<Typography component="span">App available offline.</Typography>}
+      action={
+        <IconButton
+          color="inherit"
+          onClick={(event) => {
+            Analytics.logEvent('close', 'click');
+
+            close();
+          }}
+        >
+          <Close />
+        </IconButton>
+      }
+    />
+  );
+}
+
+export default withServiceWorkerContextProvider<
+  ElementConfig<typeof React.Fragment>,
+>(ServiceWorkerStatus);
